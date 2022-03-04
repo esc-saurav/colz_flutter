721f12ed7883ea81d5b93afd83ab8d1a3a6d5df6
esc-saurav <sauravpanthi2057@gmail.com>
2022-03-04 17:17:43 +0545

    save arezzo files

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
+import 'package:catalog/pages/home_page.dart';
+import 'package:catalog/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     home: Material(
-       child: Center(
-     child: Container(
-      child: Text("Welcome to saurav"),
-     ),
-    ),
-     ),
+   
+  return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+            initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage()
+      },
     );
   }
-} 
+}
