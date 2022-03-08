37844814b5013da0555ca248122d64dde3d3e437
esc-saurav <sauravpanthi2057@gmail.com>
2022-03-08 17:51:38 +0545

    day 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 
+import 'package:catalog/widgets/drawer.dart';
 import "package:flutter/material.dart";
 
 class HomePage extends StatelessWidget {
@@ -7,16 +8,18 @@ class HomePage extends StatelessWidget {
    final String name = "saurav";
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-        body: Center(
-          child: Container(
-            child: Text("Welcome to $days days of flutter by $name"),
-          ),
+    return  SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Catalog App"),
         ),
-        drawer: Drawer(),
-      );
+          body: Center(
+            child: Container(
+              child: Text("Welcome to $days days of flutter by $name"),
+            ),
+          ),
+          drawer: MyDrawer(),
+        ),
+    );
   }
 }
